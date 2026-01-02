f35400af40b068ca091d4b5c855186c6ba367c7d
Unfavdemo <siquilward221@gmail.com>
2026-01-02 00:59:05 -0500

    Update dependencies in package.json and package-lock.json; upgrade Next.js and eslint-config-next versions, and change project title in layout.js from 'My Portfolio' to 'DemoTech'.

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 export const metadata = {
-  title: 'My Portfolio',
+  title: 'DemoTech',
   description: 'A Next.js portfolio website showcasing my projects and skills',
 }
 
